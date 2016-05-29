ec437b2a973bff26e7da85a9ffd7ff7e9bbbe9e5
周腾腾 <zhoutengteng@MacBookPro.local>
2016-05-29 22:24:41 +0800

    初步完成主界面

--- src/com/zhoutengteng/view/main.java
+++ src/com/zhoutengteng/view/main.java
@@ -2,7 +2,6 @@ package com.zhoutengteng.view;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
@@ -23,36 +22,72 @@ import javax.swing.JTextField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Vector;
 
 import javax.swing.JSpinner;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Color;
 import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListDataListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.SwingConstants;
 import java.awt.Cursor;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import java.awt.Dimension;
 import javax.swing.JTextPane;
+import javax.swing.ListModel;
 import java.awt.Point;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.ListSelectionModel;
 import javax.swing.AbstractListModel;
+import javax.swing.ComboBoxModel;
 
-public class main extends JFrame implements MouseListener {
+public class main extends JFrame implements MouseListener, ListSelectionListener {
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		// TODO Auto-generated method stub
+//        System.out.println("First index: " + e.getFirstIndex());
+//        System.out.println(", Last index: " + e.getLastIndex());
+        boolean adjust = e.getValueIsAdjusting();
+//        System.out.println(", Adjusting? " + adjust);
+        if (!adjust) {
+            JList<String> list = (JList<String>) e.getSource();
+            int selections[] = list.getSelectedIndices();
+            Object selectionValues[] = list.getSelectedValues();
+//            System.out.println(selectionValues.length + " " + selections.length);
+            for (int i = 0, n = selections.length; i < n; i++) {
+              if (i == 0) {
+                System.out.print(" Selections: ");
+              }
+              System.out.println(selections[i] + "/" + selectionValues[i] + " ");
+            }
+            JFrame other = new JFrame("item");
+            other.setContentPane(new showOneRecord()); 
+            other.setVisible(true);
+            other.setSize(this.getSize().width, this.getSize().height);
+//            other.setMaximumSize(new Dimension(400, 200));//设置最大值
+//            other.setMinimumSize(new Dimension(200, 100));//设置最小值
+            //other.getContentPane();
+        }
+	}
+
 
 	private JPanel contentPane;
 	private JLabel usernameLabel;
-	private JLabel record4;
-	private JLabel record3;
-	private JLabel record1;
-	private JLabel record2;
-	private JLabel record5;
-	private JLabel record6;
-	private JList<JLabel> list_1;
-	private JList<JLabel> list;
-	private JScrollPane scrollPane_1;
+	private JLabel afterRecord;
+	private JLabel beforeRecord;
+	private JLabel allRecord;
+	private JLabel beingRecord;
+	private JLabel hotRecord;
+	private JLabel googPeopleLabel;
+	private JList<JLabel> goodList;
+	private JList<String> recordList;
+	private JScrollPane peopellistScroll;
+	private JScrollPane recordScroll;
 
 	/**
 	 * Launch the application.
@@ -63,6 +98,7 @@ public class main extends JFrame implements MouseListener {
 				try {
 					main frame = new main();
 					frame.setVisible(true);
+					frame.setSize(1000-50, 700-50);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -83,54 +119,61 @@ public class main extends JFrame implements MouseListener {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		record4 = new JLabel("已结束活动");
-
-		
-		record3 = new JLabel("未开始活动");
-
-		
-		record1 = new JLabel("全部活动");
-
-		record2 = new JLabel("正在进行活动");
-
-		
+		afterRecord = new JLabel("已结束活动");
+		beforeRecord = new JLabel("未开始活动");
+		allRecord = new JLabel("全部活动");
+		beingRecord = new JLabel("正在进行活动");
 		usernameLabel = new JLabel("zhoutengteng");
-
+		hotRecord = new JLabel("最热门活动");
+		googPeopleLabel = new JLabel("好友列表");
+		goodList = new JList<JLabel>();
+		peopellistScroll = new JScrollPane(goodList);
 		
-		record5 = new JLabel("最热门活动");
-
 		
-		record6 = new JLabel("好友列表");
-		list_1 = new JList<JLabel>();	
-		scrollPane_1 = new JScrollPane(list_1);
-		list = new JList<JLabel>();
-		list.setVisibleRowCount(2);
-		list.setSize(new Dimension(10, 10));
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"dfsdfsdf", "2", "", "", "sd", "s", "ds", "ds", "ds", "ds", "d", "sd", "s", "ds", "d", "s", "d", "sd", "s", "d"};
+		//设置左边滚动栏
+		recordList = new JList<String>();
+		recordList.addListSelectionListener(this);
+		recordList.setVisibleRowCount(2);
+		recordList.setSize(new Dimension(10, 10));
+		recordList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		recordList.setSelectedIndex(0);
+		recordList.setEnabled(true);
+		recordList.setBorder(new LineBorder(new Color(51, 153, 153), 2));
+		recordList.setOpaque(false);
+		recordList.setRequestFocusEnabled(false);
+		recordScroll = new JScrollPane(recordList);
+		
+//		scrollPane.setPreferredSize(new Dimension(100, 100));
+//		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+//		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+//      int index = 9;
+//      list.setSelectedIndex(index);
+//      Point p = list.indexToLocation(index);
+//		JScrollBar  jScrollBar = scrollPane.getVerticalScrollBar();
+//		jScrollBar.setValue(p.y);
+//		scrollPane.setPreferredSize(new Dimension(180,2000));
+//		scrollPane.setBounds(8, 8, 310, 220);
+		recordList.setModel(new  ListModel<String>() {
+			String[] values = new String[] {"dfsdfsdf", "2", "", "", "sd", "s", "ds", "ds", "ds", "ds", "d", "sd", "s", "ds", "d", "s", "d", "sd", "s", "d", "sd", "s", "ds", "ds", "ds", "ds", "d", "sd", "s",};	
+			@Override
+			public void addListDataListener(ListDataListener l) {
+				// TODO Auto-generated method stub
+				System.out.println("ddddddddddd");
+				
+			}
+			@Override
+			public void removeListDataListener(ListDataListener l) {
+				// TODO Auto-generated method stub
+				
+			}
 			public int getSize() {
 				return values.length;
 			}
-			public Object getElementAt(int index) {
+			public String getElementAt(int index) {
 				return values[index];
 			}
-		});
-		list.setSelectedIndex(1);
-		list.setEnabled(false);
-		list.setBorder(new LineBorder(new Color(51, 153, 153), 2));
-		list.setOpaque(false);
-		list.setRequestFocusEnabled(false);
-		list.setPreferredSize(new Dimension(10, 10));
-		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-		JScrollPane scrollPane = new JScrollPane(list);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
-//		JScrollBar  jScrollBar = scrollPane.getVerticalScrollBar();
-//		jScrollBar.setValue(5);
-//		scrollPane.setPreferredSize(new Dimension(180,2000));
-//		scrollPane.setBounds(8, 8, 310, 220);
+		});
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
@@ -139,20 +182,20 @@ public class main extends JFrame implements MouseListener {
 					.addGap(32)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(record1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(allRecord, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(record2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(beingRecord, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(record3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(beforeRecord, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addGap(12)
-							.addComponent(record4, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(afterRecord, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(record5))
-						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 387, Short.MAX_VALUE))
+							.addComponent(hotRecord))
+						.addComponent(recordScroll, GroupLayout.DEFAULT_SIZE, 387, Short.MAX_VALUE))
 					.addGap(53)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 158, GroupLayout.PREFERRED_SIZE)
-						.addComponent(record6))
+						.addComponent(peopellistScroll, GroupLayout.PREFERRED_SIZE, 158, GroupLayout.PREFERRED_SIZE)
+						.addComponent(googPeopleLabel))
 					.addContainerGap())
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap(512, Short.MAX_VALUE)
@@ -165,74 +208,64 @@ public class main extends JFrame implements MouseListener {
 					.addComponent(usernameLabel)
 					.addGap(30)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(record1)
-						.addComponent(record4)
-						.addComponent(record5)
-						.addComponent(record2)
-						.addComponent(record3)
-						.addComponent(record6))
+						.addComponent(allRecord)
+						.addComponent(afterRecord)
+						.addComponent(hotRecord)
+						.addComponent(beingRecord)
+						.addComponent(beforeRecord)
+						.addComponent(googPeopleLabel))
 					.addGap(16)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE)
-						.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE))
+						.addComponent(recordScroll, GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE)
+						.addComponent(peopellistScroll, GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		
 
 		contentPane.setLayout(gl_contentPane);
-		record1.addMouseListener(this);
-		record1.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		record1.setHorizontalAlignment(SwingConstants.CENTER);
-		record1.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
-		record1.setForeground(new Color(153, 102, 0));
-		record2.addMouseListener(this);
-		record2.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		record2.setHorizontalAlignment(SwingConstants.CENTER);
-		record2.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
-		record2.setForeground(new Color(153, 102, 0));
-		record3.addMouseListener(this);
-		record3.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		record3.setHorizontalAlignment(SwingConstants.CENTER);
-		record3.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
-		record3.setForeground(new Color(153, 102, 0));
-		record4.addMouseListener(this);
-		record4.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		record4.setHorizontalAlignment(SwingConstants.CENTER);
-		record4.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
-		record4.setForeground(new Color(153, 102, 0));
-		record5.addMouseListener(this);
-		record5.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		record5.setHorizontalAlignment(SwingConstants.CENTER);
-		record5.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
-		record5.setForeground(new Color(153, 102, 0));
-		record6.addMouseListener(this);
-		record6.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		record6.setHorizontalAlignment(SwingConstants.CENTER);
-		record6.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
-		record6.setForeground(new Color(153, 102, 0));
+//		contentPane.setPreferredSize(new Dimension(100, 700));
+		
+
+
+		allRecord.addMouseListener(this);
+		allRecord.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		allRecord.setHorizontalAlignment(SwingConstants.CENTER);
+		allRecord.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
+		allRecord.setForeground(new Color(153, 102, 0));
+		beingRecord.addMouseListener(this);
+		beingRecord.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		beingRecord.setHorizontalAlignment(SwingConstants.CENTER);
+		beingRecord.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
+		beingRecord.setForeground(new Color(153, 102, 0));
+		beforeRecord.addMouseListener(this);
+		beforeRecord.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		beforeRecord.setHorizontalAlignment(SwingConstants.CENTER);
+		beforeRecord.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
+		beforeRecord.setForeground(new Color(153, 102, 0));
+		afterRecord.addMouseListener(this);
+		afterRecord.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		afterRecord.setHorizontalAlignment(SwingConstants.CENTER);
+		afterRecord.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
+		afterRecord.setForeground(new Color(153, 102, 0));
+		hotRecord.addMouseListener(this);
+		hotRecord.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		hotRecord.setHorizontalAlignment(SwingConstants.CENTER);
+		hotRecord.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
+		hotRecord.setForeground(new Color(153, 102, 0));
+		googPeopleLabel.addMouseListener(this);
+		googPeopleLabel.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		googPeopleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		googPeopleLabel.setBorder(new LineBorder(new Color(51, 204, 0), 2, true));
+		googPeopleLabel.setForeground(new Color(153, 102, 0));
 		usernameLabel.setHorizontalTextPosition(SwingConstants.CENTER);
 		
-		JLabel  jTextField = new JLabel("title0");
-		jTextField.setSize(new Dimension(100, 20));
-		jTextField.setLocation(new Point(1, 1));
-		list.add(jTextField);
-		addRecordItem();
-		
+
 
 		
 	}
 
 
-	public boolean addRecordItem() {
-		for (int i = 0;i < 30; i++) {
-			JLabel  jTextField = new JLabel("title" + i);
-			jTextField.setSize(new Dimension(100, 20));
-			jTextField.setLocation(new Point(1, 1 * 20 * (i + 1)));
-			list.add(jTextField);
-		}
-		
-		return true;
-	}
+
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
