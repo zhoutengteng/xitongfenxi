2174fd06b4724c44f209bd98a0a9a9f584cb13c6
周腾腾 <zhoutengteng@MacBookPro.local>
2016-06-10 17:12:24 +0800

    编辑一条活动记录

--- src/com/zhoutengteng/control/editRecordControl.java
+++ src/com/zhoutengteng/control/editRecordControl.java
@@ -1,19 +1,24 @@
 package com.zhoutengteng.control;
 
 import com.zhoutengteng.model.RecordModel;
+import java.io.Serializable;
 
-public class editRecordControl {
+public class editRecordControl implements Serializable {
 
 	//这里是写入数据库
 	public boolean saveOneRecord(RecordModel recordModel) {
-		
+		RecordModel record1 = new RecordModel();
+		if (record1.saveOneRecord(recordModel)) {
 		return true;
+		} else {
+			return false;
+		}
 	}
 
 	
 	
 	private boolean checkValid(RecordModel recordModel) {
-		
+		RecordModel record1 = new RecordModel();
 		return true;
 	}
 }
